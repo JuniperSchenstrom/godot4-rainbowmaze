64368fc61ea64a74303586dd4b00513c154c9842
JuniperSchenstrom <graham58000@gmail.com>
2023-11-28 20:45:49 -0500

    added enemy scene and removed broken tilemap to be implemented later

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
 # godot4-rainbowmaze
 A game about a rainbow square restoring color to a maze.
+
+Developed using Godot Engine 4.1.3.
+
+To run code:
+1. Download as .zip
+2. Install and run Godot 4.1.3
+3. Click the "Import" button and follow the instructions in the dialogue
+4. Once the project had been imported click the "Edit" button
+5. After opening the project press f5 to run it
--- player/player.gd
+++ player/player.gd
@@ -1,20 +1,17 @@
-extends Node2D
+extends CharacterBody2D
 
-const initial_position: Vector2 = Vector2(64, 64)
-const initial_scale:float = 1
 const initial_rotation_degrees:float = -90
-const speed: Vector2 = Vector2(200, 200)
+const speed: Vector2 = Vector2(100, 100)
 var direction: Vector2
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	
-	position = initial_position
-#	scale = Vector2(initial_scale, initial_scale)
 	rotation_degrees = initial_rotation_degrees
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
+func _process(_delta):
 	
 	direction = Input.get_vector("move_left", "move_right", "move_up", "move_down")
-	position += direction * speed * delta
+	velocity = direction * speed
+	move_and_slide()
