00e07a43cf3c1c7b9939adf9698d43a61ab8962d
JuniperSchenstrom <graham58000@gmail.com>
2023-11-30 23:00:03 -0500

    added maze design

--- player/player.gd
+++ player/player.gd
@@ -1,7 +1,7 @@
 extends CharacterBody2D
 
 const initial_rotation_degrees:float = -90
-const speed: Vector2 = Vector2(100, 100)
+const speed: Vector2 = Vector2(120, 120)
 var direction: Vector2
 
 # Called when the node enters the scene tree for the first time.
